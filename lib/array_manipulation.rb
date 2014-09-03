93fcb2e98c7b880c7de317e8070188868ea5993f
Anh Do <dotuanhoanganh@gmail.com>
2014-09-03 18:12:23 +1000

    tab to 2 spaces

--- lib/array_manipulation.rb
+++ lib/array_manipulation.rb
@@ -7,31 +7,31 @@ big_array = []
 9000000.times { big_array << 1 }
 
 Benchmark.bm do |bm|
-	bm.report do
-		small_array.insert 999, 0
-	end
+  bm.report do
+    small_array.insert 999, 0
+  end
 
-	bm.report do
-		big_array.insert 999, 0
-	end
+  bm.report do
+    big_array.insert 999, 0
+  end
 end
 
 Benchmark.bm do |bm|
-	bm.report do
-		small_array.push 0
-	end
+  bm.report do
+    small_array.push 0
+  end
 
-	bm.report do
-		big_array.push 0
-	end
+  bm.report do
+    big_array.push 0
+  end
 end
 
 Benchmark.bm do |bm|
-	bm.report do
-		small_array.pop
-	end
+  bm.report do
+    small_array.pop
+  end
 
-	bm.report do
-		big_array.pop
-	end
+  bm.report do
+    big_array.pop
+  end
 end
